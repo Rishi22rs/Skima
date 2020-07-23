85a10a329cda7bf7c1ed644e1ae17490ddf644f6
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2020-07-23 17:25:46 +0530

    raghav added

---
+++ src/Components/BottomNav.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { cardColorTheme } from './ColorTheme'
+
+const BottomNav = () => {
+
+    const palette = cardColorTheme[localStorage.getItem('theme')]
+
+    return (
+        <>
+            Raghav
+             
+        </>
+    )
+}
+
+export default BottomNav
