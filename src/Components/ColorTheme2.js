90aec16ee1bf8e62aafc3b3cd7fc359175d7baf0
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-08-11 21:34:54 +0530

    Store array based ColorThemes values

---
+++ src/Components/ColorTheme2.js
@@ -0,0 +1,215 @@
+export const cardColorTheme2 = {
+    Themes: [
+        {
+            Default: {
+                background: {
+                    backgroundColor: "#FFFFFF"
+                },
+                heading: {
+                    color: "#000000"
+                },
+                fontColor: {
+                    color: "#212121"
+                },
+                danger: {
+                    backgroundColor: "#FFEBEE"
+                },
+                warning: {
+                    backgroundColor: "#FFEBEE",
+                },
+                safe: {
+                    backgroundColor: "#FFFFFF"
+                },
+                safest: {
+                    backgroundColor: "#FFFFFF"
+                },
+                frontCard: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
+                },
+                bottomNav: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
+                }
+            }
+        },
+        {
+            Pastel: {
+                background: {
+                    backgroundColor: "#FFFFFF"
+                },
+                heading: {
+                    color: "#000000"
+                },
+                fontColor: {
+                    color: "#212121"
+                },
+                danger: {
+                    backgroundColor: "#FFBCBC"
+                },
+                warning: {
+                    backgroundColor: "#FFBCBC",
+                },
+                safe: {
+                    backgroundColor: "#B5EAEA"
+                },
+                safest: {
+                    backgroundColor: "#B5EAEA"
+                },
+                frontCard: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
+                },
+                bottomNav: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+                }
+            }
+        },
+        {
+            PastelDark: {
+                background: {
+                    backgroundColor: "#726A95"
+                },
+                heading: {
+                    color: "#F4EBC1"
+                },
+                fontColor: {
+                    color: "#F4EBC1"
+                },
+                danger: {
+                    backgroundColor: "#A0C1B8"
+                },
+                warning: {
+                    backgroundColor: "#A0C1B8",
+                },
+                safe: {
+                    backgroundColor: "#709FB0"
+                },
+                safest: {
+                    backgroundColor: "#709FB0"
+                },
+                frontCard: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+                },
+                bottomNav: {
+                    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+                    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+                }
+            }
+        },
+        {
+            Colorful: {
+                background: {
+                    backgroundColor: "#FFFFFF"
+                },
+                heading: {
+                    color: "black"
+                },
+                fontColor: {
+                    color: "#FFFFFF"
+                },
+                danger: {
+                    backgroundImage: "linear-gradient(to bottom right, #8C0103, #FE141A)"
+                },
+                warning: {
+                    backgroundImage: "linear-gradient(to bottom right,#FF5A15,#FFDD17)"
+                },
+                safe: {
+                    backgroundImage: "linear-gradient(to bottom right,#7B00C7,#2DCCFB)"
+                },
+                safest: {
+                    backgroundImage: "linear-gradient(to bottom right,#EC79C4,#A183F1)"
+                },
+                frontCard: {
+                    border: ""
+                }
+            }
+        },
+        {
+            Dark: {
+                background: {
+                    backgroundColor: "#121212"
+                },
+                heading: {
+                    color: "white"
+                },
+                fontColor: {
+                    color: "#FFFFFF"
+                },
+                danger: {
+                    backgroundColor: "#313131"
+                },
+                warning: {
+                    backgroundColor: "#313131",
+                },
+                safe: {
+                    backgroundColor: "#121212"
+                },
+                safest: {
+                    backgroundColor: "#121212"
+                },
+                frontCard: {
+                    border: "1px solid #414141"
+                }
+            }
+        },
+        {
+            Light: {
+                background: {
+                    backgroundColor: "#FFFFFF"
+                },
+                heading: {
+                    color: "#000000"
+                },
+                fontColor: {
+                    color: "#212121"
+                },
+                danger: {
+                    backgroundColor: "#FFEBEE"
+                },
+                warning: {
+                    backgroundColor: "#FFEBEE",
+                },
+                safe: {
+                    backgroundColor: "#FFFFFF"
+                },
+                safest: {
+                    backgroundColor: "#FFFFFF"
+                },
+                frontCard: {
+                    borderBottom: "1px solid #EEEEEE"
+                }
+            }
+        },
+        {
+            ColorfulDark: {
+                background: {
+                    backgroundColor: "#150C29"
+                },
+                heading: {
+                    color: "white"
+                },
+                fontColor: {
+                    color: "#FFFFFF"
+                },
+                danger: {
+                    backgroundImage: "linear-gradient(to bottom right, #8C0103, #FE141A)"
+                },
+                warning: {
+                    backgroundImage: "linear-gradient(to bottom right,#FF5A15,#FFDD17)"
+                },
+                safe: {
+                    backgroundImage: "linear-gradient(to bottom right,#7B00C7,#2DCCFB)"
+                },
+                safest: {
+                    backgroundImage: "linear-gradient(to bottom right,#EC79C4,#A183F1)"
+                },
+                frontCard: {
+                    border: ""
+                }
+            }
+        }
+    ]
+}
